a01d9c77a4c38c18bda771f520b87c0afc10e66f
egogoboy <egor2005sudakov@gmail.com>
2026-04-14 16:58:38 +0300

    Add Encryption.cpp implementation, change name of method and set active method

--- src/Encoding.cpp
+++ src/Encoding.cpp
@@ -4,12 +4,63 @@
 
 namespace algo {
 
-// <place_your_realization_here>
+bool validate_key(const QString& key) {
+    if (key.length() != 5) {
+        return false;
+    }
 
-bool validate_key(const QString& key) { return true; }
+    bool seen[5] = {false};
+    for (int i = 0; i < 5; ++i) {
+        if (!key[i].isDigit()) {
+            return false;
+        }
+        int digit = key[i].digitValue();
+        if (digit < 1 || digit > 5) {
+            return false;
+        }
+        if (seen[digit]) {
+            return false;
+        }
+        seen[digit] = true;
+    }
 
-QString encode(const QString& input_text, const QString& key) { return ""; }
+    return true;
+}
 
-QString decode(const QString& input_text, const QString& key) { return ""; }
+QString encode(const QString& text, const QString& key) {
+    int d = 5;
+    QString appended_text = text;
+    while (appended_text.length() % d) {
+        appended_text += "_";
+    }
 
-};  // namespace algo
+    QString result;
+
+    for (size_t i = 0; i < appended_text.length(); i += d) {
+        QString block_text;
+        for (int j = 0; j < d; ++j) {
+            block_text += appended_text.mid(i, 5)[key[j].digitValue() - 1];
+        }
+        result += block_text;
+    }
+
+    return result;
+}
+
+QString decode(const QString& text, const QString& key) {
+    int d = 5;
+
+    QString result;
+
+    for (size_t i = 0; i < text.length(); i += d) {
+        QString block_text = "00000";
+        for (int j = 0; j < d; ++j) {
+            block_text[key[j].digitValue() - 1] = text.mid(i, 5)[j];
+        }
+        result += block_text;
+    }
+
+    return result;
+}
+
+}  // namespace algo
